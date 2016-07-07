32891602ba3fbc72159490dc30fa92d841fdca6d
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-07-07 18:21:23 +0900

    Prepare tabBarController for showing userInfo

--- objc/ghUserViewer/ghUserViewer/ViewController/GUVInquiryViewController.m
+++ objc/ghUserViewer/ghUserViewer/ViewController/GUVInquiryViewController.m
@@ -9,7 +9,15 @@
     [super viewDidLoad];
 }
 
+
+- (IBAction)inquiredUserInfo:(UITextField *)sender {
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UIViewController *usersInfoTabBarController = [storyboard instantiateViewControllerWithIdentifier:@"GUVUsersInfoTabBarController"];
+    [self.navigationController pushViewController:usersInfoTabBarController animated:NO];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
+
 @end
