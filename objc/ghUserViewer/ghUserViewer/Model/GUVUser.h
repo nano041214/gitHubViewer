602ed03127069866201ef34ca2cc48d3be31c3da
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-07-08 17:06:27 +0900

    Change user model for test

--- objc/ghUserViewer/ghUserViewer/Model/GUVUser.h
+++ objc/ghUserViewer/ghUserViewer/Model/GUVUser.h
@@ -1,15 +1,20 @@
 #import <Mantle/Mantle.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface GUVUser : MTLModel
 
-@property (nonatomic, readonly) NSString *name;
+// remove readonly workaround
+@property (nonatomic) NSString *name;
 @property (nonatomic, readonly) NSURL *iconUrl;
 @property (nonatomic, readonly) NSUInteger followers;
 @property (nonatomic, readonly) NSUInteger following;
 @property (nonatomic, readonly) NSUInteger starred;
-@property (nonatomic, readonly) NSURL *blogUrl;
-@property (nonatomic, readonly) NSString *mailAddress;
-@property (nonatomic, readonly) NSString *location;
-@property (nonatomic, readonly) NSData *joinedAt;
+@property (nonatomic, readonly, nullable) NSURL *blogUrl;
+@property (nonatomic, readonly, nullable) NSString *mailAddress;
+@property (nonatomic, readonly, nullable) NSString *location;
+@property (nonatomic, readonly) NSDate *joinedAt;
+
+NS_ASSUME_NONNULL_END
 
 @end
