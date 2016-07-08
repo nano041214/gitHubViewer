07c9020bfb556c39dcff5707bc5cad63c9c5e4bb
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-07-08 14:52:36 +0900

    Add RepositoryTebleViewController

--- objc/ghUserViewer/ghUserViewer/ViewController/GUVRepositoriesViewController.h
+++ objc/ghUserViewer/ghUserViewer/ViewController/GUVRepositoriesViewController.h
@@ -1,5 +1,13 @@
+//
+//  GUVRepositoriesViewController.h
+//  ghUserViewer
+//
+//  Created by naomi-hidaka on 2016/07/08.
+//  Copyright © 2016年 naomi-hidaka. All rights reserved.
+//
+
 #import <UIKit/UIKit.h>
 
-@interface GUVRepositoriesViewController : UIViewController
+@interface GUVRepositoriesViewController : UITableViewController
 
 @end
