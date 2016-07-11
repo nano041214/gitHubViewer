a48cc227127c920d292ddd45c1e315c8350a74f5
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-07-11 18:44:26 +0900

    Link view and userInfoLables in storyboard

--- objc/ghUserViewer/ghUserViewer/View/GUVUserInfoHeaderView.m
+++ objc/ghUserViewer/ghUserViewer/View/GUVUserInfoHeaderView.m
@@ -1,9 +1,32 @@
 #import "GUVUserInfoHeaderView.h"
+#import "GUVRepositoriesViewController.h"
 
 @interface GUVUserInfoHeaderView()
+
+@property (nonatomic, weak) IBOutlet UILabel *userNameLabel;
+@property (nonatomic, weak) IBOutlet UILabel *followersLabel;
+@property (nonatomic, weak) IBOutlet UILabel *starredLabel;
+@property (nonatomic, weak) IBOutlet UILabel *followingLabel;
 
 @end
 
 @implementation GUVUserInfoHeaderView
+
+- (void)setUserNameLabel:(UILabel *)userNameLabel {
+    // get user from ReposVC...
+    userNameLabel.text = @"hoge";
+}
+
+- (void)setFollowersLabel:(UILabel *)followersLabel {
+    followersLabel.text = [NSString stringWithFormat:@"%d", 32];
+}
+
+- (void)setStarredLabel:(UILabel *)starredLabel {
+    starredLabel.text = [NSString stringWithFormat:@"%d", 43];
+}
+
+- (void)setFollowingLabel:(UILabel *)followingLabel {
+    followingLabel.text = [NSString stringWithFormat:@"%d", 93];;
+}
 
 @end
