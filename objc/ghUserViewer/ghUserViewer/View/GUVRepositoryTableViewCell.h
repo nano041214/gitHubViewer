f4277b5a4224fe731cb8404361ff6122482bf609
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-07-14 17:34:30 +0900

    Delete unnesessary xib file

---
+++ objc/ghUserViewer/ghUserViewer/View/GUVRepositoryTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  GUVRepositoryTableViewCell.h
+//  ghUserViewer
+//
+//  Created by naomi-hidaka on 2016/07/14.
+//  Copyright © 2016年 naomi-hidaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GUVRepositoryTableViewCell : UITableViewCell
+
+@end
---
+++ objc/ghUserViewer/ghUserViewer/View/GUVRepositoryTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  GUVRepositoryTableViewCell.m
+//  ghUserViewer
+//
+//  Created by naomi-hidaka on 2016/07/14.
+//  Copyright © 2016年 naomi-hidaka. All rights reserved.
+//
+
+#import "GUVRepositoryTableViewCell.h"
+
+@implementation GUVRepositoryTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
