0fc6f6d3e5fca9324989f76a8087dcafb3df879a
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-07-20 17:26:09 +0900

    Add or modify nessesary file in objc/repositories branch

--- objc/ghUserViewer/ghUserViewer/View/GUVUserInfoHeaderView.h
+++ objc/ghUserViewer/ghUserViewer/View/GUVUserInfoHeaderView.h
@@ -1,5 +1,12 @@
 #import <UIKit/UIKit.h>
+#import "GUVUser.h"
+
+NS_ASSUME_NONNULL_BEGIN
 
 @interface GUVUserInfoHeaderView : UIView
 
+@property (nonatomic, nullable) GUVUser *user;
+
 @end
+
+NS_ASSUME_NONNULL_END
