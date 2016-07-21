a714451caba2d4acd1ee6e4c26eeb2bbb9a3edb5
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-07-21 17:09:17 +0900

    Revert "Revert "[Do not merge] Objc/repositories""
    This reverts commit 599218fd09d72d23d30d478d6b6415725e3feafe.

---
+++ objc/ghUserViewer/ghUserViewer/View/GUVRepositoryTableViewCell.m
@@ -0,0 +1,19 @@
+#import "GUVRepositoryTableViewCell.h"
+#import "GUVRepository.h"
+
+@interface GUVRepositoryTableViewCell ()
+
+@property (weak, nonatomic) IBOutlet UILabel *repositoryNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *starCountLabel;
+
+@end
+
+@implementation GUVRepositoryTableViewCell
+
+- (void)setRepository:(GUVRepository *)repository {
+    _repository = repository;
+    self.repositoryNameLabel.text = repository.name;
+    self.starCountLabel.text = @"10";
+}
+
+@end
