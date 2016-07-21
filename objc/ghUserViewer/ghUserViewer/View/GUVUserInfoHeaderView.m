a004bb3decb1a6b91e8ae9764189b7304146db9c
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-07-21 14:17:15 +0900

    Reset unnesesary change of InfoHeaderView and TabController

--- objc/ghUserViewer/ghUserViewer/View/GUVUserInfoHeaderView.m
+++ objc/ghUserViewer/ghUserViewer/View/GUVUserInfoHeaderView.m
@@ -1,17 +1,9 @@
 #import "GUVUserInfoHeaderView.h"
-#import "GUVRepositoriesViewController.h"
 
 @interface GUVUserInfoHeaderView ()
-
-@property (nonatomic, weak) IBOutlet UILabel *userNameLabel;
 
 @end
 
 @implementation GUVUserInfoHeaderView
-
-- (void)setUser:(GUVUser *)user {
-    _user = user;
-    self.userNameLabel.text = user.name;
-}
 
 @end
--- objc/ghUserViewer/ghUserViewer/ViewController/GUVUserInfoTabBarController.m
+++ objc/ghUserViewer/ghUserViewer/ViewController/GUVUserInfoTabBarController.m
@@ -1,31 +1,18 @@
 #import "GUVUserInfoTabBarController.h"
 #import "FontAwesomeKit/FontAwesomeKit.h"
 #import "GUVInquiryViewController.h"
-#import "GUVRepositoriesViewController.h"
-#import "GUVUserInfoHeaderView.h"
 
 static const CGFloat IconSize = 20;
-
-@interface GUVUserInfoTabBarController ()
-
-@end
 
 @implementation GUVUserInfoTabBarController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-
-    // tab settings
     FAKFontAwesome *repositoriesIcon = [FAKFontAwesome databaseIconWithSize:IconSize];
     UITabBarItem *repositoriesItem = self.tabBar.items[0];
     repositoriesItem.image = [repositoriesIcon imageWithSize:CGSizeMake(IconSize, IconSize)];
     FAKFontAwesome *activitiesIcon = [FAKFontAwesome rssIconWithSize:IconSize];
     UITabBarItem *activitiesItem = self.tabBar.items[1];
     activitiesItem.image = [activitiesIcon imageWithSize:CGSizeMake(IconSize, IconSize)];
-}
-
--(GUVUser *)fetchUser {
-    return self.user;
 }
 
 @end
