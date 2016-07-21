b3cbf0b27308075dfe630794dcf7f2621e6d54b2
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-07-21 14:05:15 +0900

    Insert data to activity table cell

--- objc/ghUserViewer/ghUserViewer/ViewController/GUVActivitiesViewController.m
+++ objc/ghUserViewer/ghUserViewer/ViewController/GUVActivitiesViewController.m
@@ -1,9 +1,12 @@
 #import "GUVActivitiesViewController.h"
 #import "GUVUserInfoTabBarController.h"
+#import "GUVActivityTableViewCell.h"
+#import "GUVActivity.h"
 
 @interface GUVActivitiesViewController ()
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic) NSArray<GUVActivity *> *activities;
 
 @end
 
@@ -12,6 +15,9 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     [self.tableView reloadData];
+    GUVActivity *activity = [GUVActivity new];
+    activity.name = @"IssueEvent";
+    self.activities = @[activity];
 }
 
 #pragma mark - Table view data source
@@ -21,14 +27,14 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 3;
+    return self.activities.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"RepositoryCell" forIndexPath:indexPath];
-    GUVUserInfoTabBarController *userInfoTabBarController = (GUVUserInfoTabBarController *)self.tabBarController;
-    cell.textLabel.text = userInfoTabBarController.user.name;
+    GUVActivityTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ActivityCell" forIndexPath:indexPath];
+    cell.activity = self.activities[indexPath.item];
     return cell;
+
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
