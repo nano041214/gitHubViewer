591b8de590854dd290f452ea59397670f7590dc4
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-08-08 13:36:16 +0900

    Insert image and imformation to UserInfoHeaderView

--- objc/ghUserViewer/ghUserViewer/View/GUVUserInfoHeaderView.m
+++ objc/ghUserViewer/ghUserViewer/View/GUVUserInfoHeaderView.m
@@ -1,9 +1,14 @@
 #import "GUVUserInfoHeaderView.h"
 #import "GUVRepositoriesViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface GUVUserInfoHeaderView ()
 
 @property (nonatomic, weak) IBOutlet UILabel *userNameLabel;
+@property (nonatomic, weak) IBOutlet UIImageView *userIconImageView;
+@property (nonatomic, weak) IBOutlet UILabel *followingCountLabel;
+@property (nonatomic, weak) IBOutlet UILabel *followersCountLabel;
+@property (nonatomic, weak) IBOutlet UILabel *starredCountLabel;
 
 @end
 
@@ -12,6 +17,11 @@
 - (void)setUser:(GUVUser *)user {
     _user = user;
     self.userNameLabel.text = user.name;
+    [self.userIconImageView sd_setImageWithURL:user.iconURL
+                              placeholderImage:[UIImage imageNamed:@"octcat"]];
+    self.followersCountLabel.text = [@(user.followersCount) stringValue];
+    self.followingCountLabel.text = [@(user.followingCount) stringValue];
+    self.starredCountLabel.text = [@(user.starredCount) stringValue];
 }
 
 @end
