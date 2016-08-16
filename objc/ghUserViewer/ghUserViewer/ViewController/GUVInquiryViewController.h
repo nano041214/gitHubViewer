e3f81dbfbaec6fce7b8286bf6065e8aa7270d84d
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-08-16 17:45:33 +0900

    [Refactoring] Add pragma mark

--- objc/ghUserViewer/ghUserViewer/ViewController/GUVInquiryViewController.h
+++ objc/ghUserViewer/ghUserViewer/ViewController/GUVInquiryViewController.h
@@ -1,9 +1,9 @@
 #import <UIKit/UIKit.h>
 #import "GUVUser.h"
 
-NS_ASSUME_NONNULL_BEGIN
-
 @class GUVInquiryViewController;
+
+NS_ASSUME_NONNULL_BEGIN
 
 @protocol GUVInquiryViewControllerDelegate <NSObject>
 
