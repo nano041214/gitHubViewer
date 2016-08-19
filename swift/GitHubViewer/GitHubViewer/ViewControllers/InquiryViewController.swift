5610a15a22e02c798fb2cb92bca06fb2713f6f12
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-08-19 18:30:55 +0900

    Add segue to tabBarController

--- swift/GitHubViewer/GitHubViewer/ViewControllers/InquiryViewController.swift
+++ swift/GitHubViewer/GitHubViewer/ViewControllers/InquiryViewController.swift
@@ -2,5 +2,10 @@ import UIKit
 
 class InquiryViewController: UIViewController {
 
+    @IBAction func didEnterUserName(sender: AnyObject) {
+        let tabBarController = self.storyboard?.instantiateViewControllerWithIdentifier("UserInfoTabBarController") as! UserInfoTabBarController
+        showViewController(tabBarController, sender: self)
+    }
+
 }
 
