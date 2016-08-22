4bc0ffe24ffe1c50f6f937c6665f058efed759dc
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-08-22 17:08:01 +0900

    Use guard to cast

--- swift/GitHubViewer/GitHubViewer/ViewControllers/InquiryViewController.swift
+++ swift/GitHubViewer/GitHubViewer/ViewControllers/InquiryViewController.swift
@@ -1,9 +1,10 @@
 import UIKit
 
 class InquiryViewController: UIViewController {
-
-    @IBAction func didEnterUserName(sender: AnyObject) {
-        let tabBarController = self.storyboard?.instantiateViewControllerWithIdentifier("UserInfoTabBarController") as! UserInfoTabBarController
+    @IBAction func didEnterUserName(sender: UITextField) {
+        guard let tabBarController = storyboard?.instantiateViewControllerWithIdentifier("UserInfoTabBarController") as? UserInfoTabBarController else {
+            fatalError("Could not load UserInfoTabBarController")
+        }
         showViewController(tabBarController, sender: self)
     }
 }
