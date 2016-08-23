cc0290beeff6d769e2e58024c64b9cc89734068c
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-08-23 15:33:52 +0900

    Refactor using const val and switch function

--- swift/GitHubViewer/GitHubViewer/ViewControllers/RepositoriesViewController.swift
+++ swift/GitHubViewer/GitHubViewer/ViewControllers/RepositoriesViewController.swift
@@ -1,33 +1,42 @@
 import UIKit
 
 class RepositoriesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let defaultRowHeight: CGFloat = 100.0
+    let sectionCounts = 2
+
+    // define value workaround
+    let repositoriesCount = 5
+
     @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.estimatedRowHeight = 100.0
+        self.tableView.estimatedRowHeight = defaultRowHeight
+        // ajust row height depends on its content
         self.tableView.rowHeight = UITableViewAutomaticDimension
     }
 
-    // MARK - tableViewDataSource
+    // MARK: - tableViewDataSource
 
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 2;
+        return sectionCounts;
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int  {
-        if section == 0 {
+        switch section {
+        case 0:
             return 1
-        } else {
-            return 5
+        default:
+            return repositoriesCount
         }
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
+        switch indexPath.section {
+        case 0:
             let cell = tableView.dequeueReusableCellWithIdentifier("UserInfoCell", forIndexPath: indexPath)
             return cell
-        } else {
+        default:
             let cell = tableView.dequeueReusableCellWithIdentifier("RepositoryCell", forIndexPath: indexPath)
             return cell
         }
