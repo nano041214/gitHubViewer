1bc0fc9ca6d74446ac9591c4cdb8ea7131e9ec44
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-08-25 14:25:49 +0900

    User enum

--- swift/GitHubViewer/GitHubViewer/ViewControllers/ActivitiesViewController.swift
+++ swift/GitHubViewer/GitHubViewer/ViewControllers/ActivitiesViewController.swift
@@ -1,27 +1,36 @@
 import UIKit
 
 class ActivitiesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    enum ActivitiesTableCellType: Int {
+        case UserInfo
+        case Activity
+    }
+
+    let sectionCount = 2
+
+    // define value workaround
+    let activitiesCount = 5
 
     @IBOutlet weak var tableView: UITableView!
     
     // MARK: - tableViewDataSource
 
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 2;
+        return sectionCount;
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int  {
         switch section {
-        case 0:
+        case ActivitiesTableCellType.UserInfo.rawValue:
             return 1
         default:
-            return 3
+            return activitiesCount
         }
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath:NSIndexPath) -> UITableViewCell {
         switch indexPath.section {
-        case 0:
+        case ActivitiesTableCellType.UserInfo.rawValue:
             let cell = tableView.dequeueReusableCellWithIdentifier("UserInfoCell", forIndexPath: indexPath)
             return cell
         default:
@@ -32,7 +41,7 @@ class ActivitiesViewController: UIViewController, UITableViewDelegate, UITableVi
 
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         switch indexPath.section {
-        case 0:
+        case ActivitiesTableCellType.UserInfo.rawValue:
             return 139.5
         default:
             return 96.5
