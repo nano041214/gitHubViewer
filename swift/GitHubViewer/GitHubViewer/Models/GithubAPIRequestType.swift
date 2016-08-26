aca1b8eb54af77a0be77020d60ff234adbf2bfe6
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-08-26 13:34:31 +0900

    Add API session model

---
+++ swift/GitHubViewer/GitHubViewer/Models/GithubAPIRequestType.swift
@@ -0,0 +1,13 @@
+//
+//  GithubAPIRequestType.swift
+//  GitHubViewer
+//
+//  Created by naomi-hidaka on 2016/08/26.
+//  Copyright © 2016年 naomi-hidaka. All rights reserved.
+//
+
+import Cocoa
+
+class GithubAPIRequestType: RequestType {
+
+}
---
+++ swift/GitHubViewer/GitHubViewer/Models/GithubAPISession.swift
@@ -0,0 +1,5 @@
+import APIKit
+
+class GithubAPISession: Session {
+    
+}
