1369345603174d5d52ed0a01da227d2a0622cdb2
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-08-29 11:34:25 +0900

    Accept user name from textfield

--- swift/GitHubViewer/GitHubViewer/ViewControllers/InquiryViewController.swift
+++ swift/GitHubViewer/GitHubViewer/ViewControllers/InquiryViewController.swift
@@ -4,23 +4,25 @@ import APIKit
 class InquiryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        let request = UserRequest(userName: "nano041214")
+    @IBAction func didEnterUserName(sender: UITextField) {
+        guard let userNameString: String = sender.text else {
+            fatalError("it's not string")
+        }
+        let request = UserRequest(userName: userNameString)
         Session.sendRequest(request) { result in
             switch result {
             case .Success(let response):
+                guard let tabBarController = self.storyboard?.instantiateViewControllerWithIdentifier("UserInfoTabBarController") as? UserInfoTabBarController else {
+                    fatalError("Could not load UserInfoTabBarController")
+                }
+                self.showViewController(tabBarController, sender: self)
                 print(response)
             case .Failure(let error):
                 print(error)
 
             }
         }
-    }
-
-    @IBAction func didEnterUserName(sender: UITextField) {
-        guard let tabBarController = storyboard?.instantiateViewControllerWithIdentifier("UserInfoTabBarController") as? UserInfoTabBarController else {
-            fatalError("Could not load UserInfoTabBarController")
-        }
-        showViewController(tabBarController, sender: self)
     }
 }
