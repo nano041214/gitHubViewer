8dfdd588eac082d100525a254462bdf90481756c
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-08-29 18:38:33 +0900

    Delete debug pring

--- swift/GitHubViewer/GitHubViewer/ViewControllers/UserInfoTabBarController.swift
+++ swift/GitHubViewer/GitHubViewer/ViewControllers/UserInfoTabBarController.swift
@@ -2,10 +2,4 @@ import UIKit
 
 class UserInfoTabBarController: UITabBarController {
     var user: User?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        print(user)
-    }
 }
