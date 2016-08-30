8a37baf393db85c06fa46f76d00d36a1dd75d373
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-08-30 15:01:22 +0900

    Add outlet to UserInfoTableViewCell

--- swift/GitHubViewer/GitHubViewer/Views/UserInfoTableViewCell.swift
+++ swift/GitHubViewer/GitHubViewer/Views/UserInfoTableViewCell.swift
@@ -2,6 +2,24 @@ import UIKit
 
 class UserInfoTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var followedLabel: UILabel!
+    @IBOutlet weak var followingLabel: UILabel!
+    @IBOutlet weak var starredLabel: UILabel!
+
     static let height: CGFloat = 140
+
+    var userProvider: UserProvider! {
+        get {
+            return self.userProvider
+        }
+
+        set {
+            self.userNameLabel.text = userProvider.user.name
+        }
+    }
+
+
 
 }
