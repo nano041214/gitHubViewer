6b4eb86ecc2f6391c2551a7b19e4a761defa029e
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-08-30 11:33:24 +0900

    Add default user

--- swift/GitHubViewer/GitHubViewer/ViewControllers/UserInfoTabBarController.swift
+++ swift/GitHubViewer/GitHubViewer/ViewControllers/UserInfoTabBarController.swift
@@ -1,9 +1,16 @@
 import UIKit
 
 class UserInfoTabBarController: UITabBarController, userProvider {
-    var user: User!
+    var user: User = User(name: "cookpad",
+                          iconURL: NSURL(string: "https://avatars.githubusercontent.com/u/119195?v=3")!,
+                          followersCount: 0,
+                          followingCount: 0,
+                          blogURL: nil,
+                          mailAddress: nil,
+                          location: nil,
+                          joinedDate: NSDate())
 }
 
 protocol userProvider {
-    var user: User! { get }
+    var user: User { get }
 }
