dbf220b6ed64e8015bcab4af672a85ae1f03e1a6
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-08-30 14:14:19 +0900

    Stop creating default user

--- swift/GitHubViewer/GitHubViewer/ViewControllers/UserInfoTabBarController.swift
+++ swift/GitHubViewer/GitHubViewer/ViewControllers/UserInfoTabBarController.swift
@@ -1,16 +1,9 @@
 import UIKit
 
 class UserInfoTabBarController: UITabBarController, userProvider {
-    var user: User = User(name: "cookpad",
-                          iconURL: NSURL(string: "https://avatars.githubusercontent.com/u/119195?v=3")!,
-                          followersCount: 0,
-                          followingCount: 0,
-                          blogURL: nil,
-                          mailAddress: nil,
-                          location: nil,
-                          joinedDate: NSDate())
+    var user: User!
 }
 
 protocol userProvider {
-    var user: User { get }
+    var user: User! { get }
 }
