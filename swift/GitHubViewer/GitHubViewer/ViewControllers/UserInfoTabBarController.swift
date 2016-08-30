8751a70178fb3b7a5841e81525e0d9c055fadd3c
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-08-30 14:28:52 +0900

    Delete connection in story board and connect in code

--- swift/GitHubViewer/GitHubViewer/ViewControllers/UserInfoTabBarController.swift
+++ swift/GitHubViewer/GitHubViewer/ViewControllers/UserInfoTabBarController.swift
@@ -2,6 +2,25 @@ import UIKit
 
 class UserInfoTabBarController: UITabBarController, userProvider {
     var user: User!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let repositoriesViewController = self.storyboard?.instantiateViewControllerWithIdentifier("RepositoriesViewController") as? RepositoriesViewController else {
+            fatalError("Could not load RepositoriesViewController")
+        }
+        guard let activitiesViewController = self.storyboard?.instantiateViewControllerWithIdentifier("ActivitiesViewController") as? ActivitiesViewController else {
+            fatalError("Could not load ActivitiesViewController")
+        }
+
+        let repositoriesContainerController = UINavigationController(rootViewController: repositoriesViewController)
+        repositoriesContainerController.title = "Repos"
+        let activitiesContainerController = UINavigationController(rootViewController: activitiesViewController)
+        activitiesContainerController.title = "Activities"
+
+        self.viewControllers = [repositoriesContainerController, activitiesContainerController]
+    }
+
 }
 
 protocol userProvider {
