d0edfc09f4868e377b81104194e55fd6261f3146
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-09-09 18:13:15 +0900

    Connect to storyboard

--- GitHubViewer/Views/RepositoryTableViewCell.swift
+++ GitHubViewer/Views/RepositoryTableViewCell.swift
@@ -3,13 +3,16 @@ import UIKit
 class RepositoryTableViewCell: UITableViewCell {
 
     static let height: CGFloat = 97
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var starredCountLabel: UILabel!
 
     var repository: Repository? {
         didSet {
             guard let fetchedRepository = repository else {
                 return
             }
-            textLabel?.text = fetchedRepository.name
+            nameLabel.text = fetchedRepository.name
+            starredCountLabel.text = String(fetchedRepository.starredCount)
         }
     }
 }
