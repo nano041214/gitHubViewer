c46c939ad8bb1ee1ef3af311dd0beeccf07f4c30
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-09-15 14:08:55 +0900

    Connect with storyboard

--- GitHubViewer/Views/ActivityTableViewCell.swift
+++ GitHubViewer/Views/ActivityTableViewCell.swift
@@ -3,5 +3,17 @@ import UIKit
 class ActivityTableViewCell: UITableViewCell {
 
     static let height: CGFloat = 97
+    @IBOutlet weak var activityNameLabel: UILabel!
+    @IBOutlet weak var repositoryNameLabel: UILabel!
+
+    var activity: Activity? {
+        didSet {
+            guard let activity = activity else {
+                return
+            }
+            activityNameLabel.text = activity.name
+            repositoryNameLabel.text = activity.descriptionString
+        }
+    }
 
 }
