7c1c06422fe71d93a7fb4c54175ae96e1bedf07b
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-09-20 17:13:18 +0900

    Add decode helper

--- GitHubViewer/Models/Repository.swift
+++ GitHubViewer/Models/Repository.swift
@@ -31,23 +31,6 @@ struct Repository {
 }
 
 extension Repository: Decodable {
-    static let URLTransformer = Transformer<String, NSURL> { URLString throws -> NSURL in
-        guard let URL = NSURL(string: URLString) else {
-            throw customError("Invalid URL string: \(URLString)")
-        }
-        return URL
-    }
-
-    static let dateTransformer = Transformer<String, NSDate> { dateString throws -> NSDate in
-        let formatter: NSDateFormatter = NSDateFormatter()
-        formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
-        guard let date = formatter.dateFromString(dateString) else {
-            throw customError("Invalid Date String: \(dateString)")
-        }
-        return date
-    }
-
     static func decode(extractor: Extractor) throws -> Repository {
         return try Repository(name: extractor <| "name",
                               userName: extractor <| ["owner", "login"],
@@ -60,8 +43,8 @@ extension Repository: Decodable {
                               commitsCount: 0,
                               issuesCount: extractor <| "open_issues_count",
                               starredCount: extractor <| "stargazers_count",
-                              repositoryURL: URLTransformer.apply(extractor <| "html_url"),
-                              createdDate: dateTransformer.apply(extractor <| "created_at"),
-                              updatedDate: dateTransformer.apply(extractor <| "updated_at"))
+                              repositoryURL: DecodeHelper.URLTransformer.apply(extractor <| "html_url"),
+                              createdDate: DecodeHelper.dateTransformer.apply(extractor <| "created_at"),
+                              updatedDate: DecodeHelper.dateTransformer.apply(extractor <| "updated_at"))
     }
 }
--- GitHubViewer/Models/User.swift
+++ GitHubViewer/Models/User.swift
@@ -13,31 +13,14 @@ struct User {
 }
 
 extension User: Decodable {
-    static let URLTransformer = Transformer<String, NSURL> { URLString throws -> NSURL in
-        guard let URL = NSURL(string: URLString) else {
-            throw customError("Invalid URL string: \(URLString)")
-        }
-        return URL
-    }
-
-    static let dateTransformer = Transformer<String, NSDate> { dateString throws -> NSDate in
-        let formatter: NSDateFormatter = NSDateFormatter()
-        formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
-        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
-        guard let date = formatter.dateFromString(dateString) else {
-            throw customError("Invalid Date String: \(dateString)")
-        }
-        return date
-    }
-
     static func decode(extractor: Extractor) throws -> User {
         return try User(name: extractor <| "login",
-                        iconURL: URLTransformer.apply(extractor <| "avatar_url"),
+                        iconURL: DecodeHelper.URLTransformer.apply(extractor <| "avatar_url"),
                         followersCount: extractor <| "followers",
                         followingCount: extractor <| "following",
-                        blogURL: URLTransformer.apply(extractor <|? "blog"),
+                        blogURL: DecodeHelper.URLTransformer.apply(extractor <|? "blog"),
                         mailAddress: extractor <|? "email",
                         location: extractor <|? "location",
-                        joinedDate: dateTransformer.apply(extractor <| "created_at"))
+                        joinedDate: DecodeHelper.dateTransformer.apply(extractor <| "created_at"))
     }
 }
