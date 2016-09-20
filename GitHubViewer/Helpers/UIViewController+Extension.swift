fd003ebcde9181261565e1a6d6d01588a18d6bb7
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-09-20 17:22:21 +0900

    Refactor nits

--- GitHubViewer/Helpers/UIViewController+Extension.swift
+++ GitHubViewer/Helpers/UIViewController+Extension.swift
@@ -4,7 +4,7 @@ extension UIViewController {
     func ghv_instantiateViewController<ViewControllerType: UIViewController>() -> ViewControllerType {
         let className = String(ViewControllerType)
         guard let viewController = storyboard?.instantiateViewControllerWithIdentifier(className) as? ViewControllerType else {
-            fatalError("Could not load \(className.self)")
+            fatalError("Could not load \(className)")
         }
         return viewController
     }
