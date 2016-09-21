063e98a62e2964fd454220caca31fdfabd87feec
naomi-hidaka <naomi-hidaka@cookpad.com>
2016-09-21 13:54:26 +0900

    Add GitHubLinkViewDelegate

--- GitHubViewer/Views/GitHubLinkView.swift
+++ GitHubViewer/Views/GitHubLinkView.swift
@@ -2,4 +2,13 @@ import UIKit
 
 class GitHubLinkView: UITableViewHeaderFooterView {
     static let height: CGFloat = 65.0
+    var delegate: GitHubLinkViewDelegate?
+
+    @IBAction func didTapLinkViewButton(sender: AnyObject) {
+        delegate?.didTapLinkViewButton()
+    }
+}
+
+protocol GitHubLinkViewDelegate {
+    func didTapLinkViewButton()
 }
